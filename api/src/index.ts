1749e5ff6135fcbdb5afc63b820028b0442d6529
Liana Mancini <liana.mancini@gmail.com>
2023-10-18 15:09:23 -0400

    CRUD endpoints

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,18 +1,129 @@
-const express = require('express')
-const cors = require('cors');
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { prisma } from './prismaClient';
+import { tarotCards } from './seed';
+
 const app = express()
+
 const port = 8080
 
 app.use(cors());
+app.use(express.json())   
 
-app.get('/', (req: any, res: any) => {
-  res.send('Some change!')
-})
+// READ
+app.get('/', (req: Request, res: Response) => {
+  (async () => {
+    try {
+      const spreads = await prisma.tarotSpread.findMany()
+      return res.send({
+          success: true,
+          data: spreads
+      });
+    } catch (error) {
+      console.log(error)
+      return res.json({
+          success: false,
+          message: error
+      });
+    }
+  })()})
 
-app.post('/api/signup', (req: any, res: any) => {
-  console.log("hit route");
-  res.status(404).send({error: Error})
-})
+type TarotCard = typeof tarotCards[number]['name'];
+
+// CREATE
+app.post('/api/spread/create', (req: Request, res: Response) => {
+  console.log(req.body);
+  (async () => {
+    try {
+      const { name, notes, cards} = req.body;
+
+      const newSpread = await prisma.tarotSpread.create({
+        data: {
+          name,
+          notes,
+          cards: {
+            create: cards.map((cardName: TarotCard) => ({
+              name: cardName,
+            })),
+          }
+        },
+        include: {cards: true}
+      });
+
+      return res.send({
+          success: true,
+          data: newSpread
+      });
+    } catch (error) {
+      console.log(error)
+      return res.json({
+          success: false,
+          message: error
+      });
+    }
+  })()})
+
+  // UPDATE
+  app.put('/api/spread/:id', (req: Request, res: Response) => {
+    console.log(req.body);
+    const { id } = req.params;
+    (async () => {
+      try {
+        const { name, notes, cards} = req.body;
+
+        const updateSpread = await prisma.tarotSpread.update({
+          where: {
+            id: parseInt(id),
+          },
+          data: {
+            name,
+            notes,
+            cards: {
+              create: cards?.map((cardName: TarotCard) => ({
+                name: cardName,
+              })),
+            }
+          },
+          include: {cards: true}
+        })
+  
+        return res.send({
+            success: true,
+            data: updateSpread
+        });
+      } catch (error) {
+        console.log(error)
+        return res.json({
+            success: false,
+            message: error
+        });
+      }
+    })()})
+
+  // DELETE
+  app.delete('/api/spread/:id', (req: Request, res: Response) => {
+    console.log(req.body);
+    const { id } = req.params;
+    (async () => {
+      try {
+        const deleteSpread = await prisma.tarotSpread.delete({
+          where: {
+            id: parseInt(id)
+          },
+        })
+  
+        return res.send({
+            success: true,
+            data: deleteSpread
+        });
+      } catch (error) {
+        console.log(error)
+        return res.json({
+            success: false,
+            message: error
+        });
+      }
+    })()})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
